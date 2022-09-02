cec68c9cae17a1e5ed3a967df056e68233cd551e
jp2137 <sbaiyassine2004@gmail.com>
2022-09-02 16:53:40 +0200

    drugi

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.button1).setOnClickListener {
+            if(findViewById<ImageView>(R.id.imageView).isVisible === true) {
+                findViewById<ImageView>(R.id.imageView).visibility = View.INVISIBLE
+                findViewById<Button>(R.id.button1).text = "pokaz"
+            }
+            else if(findViewById<ImageView>(R.id.imageView).isVisible === false) {
+                findViewById<ImageView>(R.id.imageView).visibility = View.VISIBLE
+                findViewById<Button>(R.id.button1).text = "ukryj"
+            }
+        }
     }
 }
